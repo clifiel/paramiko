f16f1e9048e90572dc6b9519b0b22c3c9c4a1800
clifiel <clifiel@gmail.com>
2017-09-24 01:21:28 -0300

    inicial

---
+++ olamundo.py
@@ -0,0 +1,25 @@
+#!/home/cbernaldo/docs/NetworkAutomation/Projetos/paramiko/bin/python
+
+#Require Python 3.3
+
+import paramiko;
+
+cli = paramiko.client.SSHClient();
+cli.set_missing_host_key_policy(paramiko.client.AutoAddPolicy());
+cli.connect(hostname="192.168.1.2", username="cbernaldo",password="123456");
+#cli.connect(hostname="192.168.1.2", username="cbernaldo");
+sdtin, stdout, stderr = cli.exec_command("ls -l ~ | awk '{print $9}'");
+stdout.channel.recv_exit_status();
+
+for line in stdout:
+	print(line);
+
+#print(type(stdout));
+
+sdtin, stdout, stderr = cli.exec_command("pwd");
+stdout.channel.recv_exit_status();
+lines = stdout.readlines();
+for line in lines:
+	print(line);
+
+cli.close();	
